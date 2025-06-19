eb3c6652fe81ad2eff9dc542da6efb5df68f6a57
Tristan Davis <tridav95@gmail.com>
2025-06-19 18:57:55 -0500

    Unit test with intentional flaw.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetApiKey(t *testing.T) {
+	headers := http.Header{
+    	"Content-Type":      {"application/json"},
+	}
+	headers.Set("Authorization", "ApiKey your-token-here")
+	_, err  := GetAPIKey(headers)
+	if (err != nil) {
+		t.Errorf("Unexpected nil %v",err)
+	}
+
+	headers.Set("Authorization", "BearerToken your-token-here")
+	_, err2 := GetAPIKey(headers)
+	if (err2 != nil) {
+		t.Errorf("Expected error when invalid key given: %v",err2)
+	}
+}
